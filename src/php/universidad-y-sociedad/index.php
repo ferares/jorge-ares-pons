aaaf57dd627f183f8cc56ebb8a4cb85b2a4a7c48
ferares <fermin@ares.uy>
2021-08-05 08:09:17 -0300

    fix invalid require_once

--- src/php/universidad-y-sociedad/index.php
+++ src/php/universidad-y-sociedad/index.php
@@ -134,6 +134,6 @@
     ],
   ];
 ?>
-<?php require_once('../inc/_head.php'); require_once('../inc/_header.php'); ?>
+<?php require_once('../functions.php'); require_once('../inc/_head.php'); require_once('../inc/_header.php'); ?>
 <?php require_once('../inc/document-list.php') ?>
 <?php require_once('../inc/_footer.php'); ?>
