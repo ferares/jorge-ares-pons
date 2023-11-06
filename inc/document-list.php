82dbac70b37eaaf13ecfea312b19850753da9d1c
ferares <fermin@ares.uy>
2023-11-05 21:13:46 -0300

    Update document titles/descriptions Remove duplicates Add dates Single line for document lists

--- inc/document-list.php
+++ inc/document-list.php
@@ -17,6 +17,14 @@
                 <?php echo $document['description'] ?>
               <?php endif; ?>
             </p>
+            <div class="document-list__item__date">
+              <?php if ($document['date']): ?>
+                <img class="document-list__date__img" src="<?= assetPath('icons/calendar.png') ?>" alt="">
+                <span>
+                  <?php echo $document['date'] ?>
+                </span>
+              <?php endif; ?>
+            </div>
           </div>
         </div>
         <a class="btn document-list__item__link" href='<?= assetPath("documents/$sectionPath/$documentName") ?>' target="_blank">
